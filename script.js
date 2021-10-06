04218d82bed843346973b27199d5cdd4ded2e588
zjaki <dzjajic20@gmail.com>
2021-10-06 17:23:10 +0200

    add bg2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function changeBg1(){
     document.body.style.background = "linear-gradient(45deg, #3f87a6, #ebf8e1, #f69d3c)";
 }
+function changeBg2(){
+    document.body.style.background = "linear-gradient(0.25turn, #22577A, #38A3A5, #57CC99, #80ED99)";
+}
